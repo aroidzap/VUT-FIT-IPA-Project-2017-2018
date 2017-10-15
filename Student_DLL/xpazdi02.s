1d7094fe399b1e47b1e79d82c9a9d848e8594ccf
Tomáš Pazdiora <tomas.pazdiora@gmail.com>
2017-10-15 16:09:18 +0200

    Add function header comment

--- Student_DLL/xpazdi02.s
+++ Student_DLL/xpazdi02.s
@@ -24,7 +24,15 @@ DllMain:
 	pop ebp
 	mov eax,dword 1
 	ret 12
-		
+
+
+; void ipa_algorithm(
+;		unsigned char *input_data, 
+;		unsigned char *output_data, 
+;		unsigned int width, 
+;		unsigned int height, 
+;		int argc, char** argv	);	
+
 ipa_algorithm:
 	push ebp
 	mov ebp,esp
